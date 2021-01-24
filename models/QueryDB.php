4088d588a154c5b80a696100469f3d92ccdb6000
Maykl <maicolhakobyan853@yahoo.it>
2021-01-24 22:40:03 +0100

    correzione bug id

--- models/QueryDB.php
+++ models/QueryDB.php
@@ -12,27 +12,35 @@ class QueryDB
         $csvValues = new CsvImporter;
         $row = $csvValues->ReaderCSV();
         
-        $stmt = Connection::connect()->prepare(
-            "INSERT INTO $this->table(nome,cognome,email,note) VALUES(:n,:c,:e,:nt)"
-        );
+        $stmt = Connection::connect()->prepare("SELECT * FROM $this->table");
+        $stmt->execute();
+        $db = $stmt->fetch(PDO::FETCH_ASSOC);
 
-        foreach ($row as $r):
-            try 
-            {                
-                if(empty($r['email'])):
-                    $r['email'] = $r['id'] . $r['nome'] . 'email@gmail.com';
-                endif;
-                $stmt->bindParam(':n',$r['nome'], PDO::PARAM_STR);
-                $stmt->bindParam(':c',$r['cognome'], PDO::PARAM_STR);
-                $stmt->bindParam(':e',$r['email'], PDO::PARAM_STR);
-                $stmt->bindParam(':nt',$r['note'] , PDO::PARAM_STR);
-                $stmt->execute();
-            } 
-            catch (PDOException $e)
-            {
-                echo $e->getMessage();
-            }
-        endforeach;
+        // mettere un controllo se > 0!
+        if($db > 0): 
+            return;
+        else: 
+            $stmt = Connection::connect()->prepare(
+                "INSERT INTO $this->table(nome,cognome,email,note) VALUES(:n,:c,:e,:nt)"
+            );
+            foreach ($row as $r):
+                try 
+                {                
+                    if(empty($r['email'])):
+                        $r['email'] = $r['id'] . $r['nome'] . 'email@gmail.com';
+                    endif;
+                    $stmt->bindParam(':n',$r['nome'], PDO::PARAM_STR);
+                    $stmt->bindParam(':c',$r['cognome'], PDO::PARAM_STR);
+                    $stmt->bindParam(':e',$r['email'], PDO::PARAM_STR);
+                    $stmt->bindParam(':nt',$r['note'] , PDO::PARAM_STR);
+                    $stmt->execute();
+                } 
+                catch (PDOException $e)
+                {
+                    echo $e->getMessage();
+                }
+            endforeach;
+        endif;
     }
 
     public function selectQuery() : array 
